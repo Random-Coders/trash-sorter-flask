98f0cf4347798824e583b12975b4c15b178d89fb
RafaelCenzano <savagecoder77@gmail.com>
2019-03-10 00:40:12 -0800

    make changes for google cloud platform

--- run.py
+++ run.py
@@ -2,5 +2,6 @@ from trash import app
 
 app.run(
         debug=True,
-        port=9000
+        port=9000,
+        host='0.0.0.0'
         )
--- trash/views.py
+++ trash/views.py
@@ -3,10 +3,13 @@ from flask import render_template, redirect, url_for, request
 from PIL import Image
 from os import path
 
+@app.route("/", methods=["GET"])
+def home():
+    return "hello there"
 
 @app.route("/upload", methods=["POST"])
 def upload():
     img = Image.open(request.files['file'])
     f = path.join(app.config['UPLOAD_FOLDER'], 'image.png')
     img.save(f)
-    return 'Success!'
+    return 'Image saved Success!'
