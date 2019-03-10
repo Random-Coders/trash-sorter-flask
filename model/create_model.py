2db6a114522de218ce44eb96f6cfde792aa4c72e
calee14 <caplee1000@gmail.com>
2019-03-09 22:22:35 -0800

    adding code to create the model

--- model/create_model.py
+++ model/create_model.py
@@ -2,4 +2,4 @@ from clarifai.rest import ClarifaiApp
 import constants
 
 app = ClarifaiApp(api_key=constants.CLARIFAI_API)
-model = app.models.create('rafs', concepts=['boscoe'])
+model = app.models.create('trashsorter', concepts=['cardboard', 'glass', 'metal', 'paper', 'plastic', 'trash'])
