bbb3d60b50bd6a1a5bb557ed28357be0eef5c44f
calee14 <caplee1000@gmail.com>
2019-03-09 22:08:11 -0800

    uncommenting code so all images can be added at the same time

--- model/add_concepts.py
+++ model/add_concepts.py
@@ -16,53 +16,53 @@ def chunks(l, n):
 
 # add cardboard
 # cardboard files
-# cardboard_imgs = os.listdir('data/train/cardboard')
-# cardboard_imgs = list(chunks(cardboard_imgs, 128))
-# for split_cardboard_imgs in cardboard_imgs:
-# 	imgs = []
-# 	for img_name in split_cardboard_imgs:
-# 		print(img_name)
-# 		if img_name == ".DS_Store":
-# 			continue
-# 		img = ClImage(filename=f"data/train/cardboard/{img_name}", concepts=["cardboard"], not_concepts=["glass", "metal", "plastic", "trash"])
-# 		imgs.append(img)
-# 	app.inputs.bulk_create_images(imgs)
+cardboard_imgs = os.listdir('data/train/cardboard')
+cardboard_imgs = list(chunks(cardboard_imgs, 128))
+for split_cardboard_imgs in cardboard_imgs:
+	imgs = []
+	for img_name in split_cardboard_imgs:
+		print(img_name)
+		if img_name == ".DS_Store":
+			continue
+		img = ClImage(filename=f"data/train/cardboard/{img_name}", concepts=["cardboard"], not_concepts=["glass", "metal", "plastic", "trash"])
+		imgs.append(img)
+	app.inputs.bulk_create_images(imgs)
 
-# glass_imgs = os.listdir('data/train/glass')
-# glass_imgs = list(chunks(glass_imgs, 128))
-# for split_glass_imgs in glass_imgs:
-# 	imgs = []
-# 	for img_name in split_glass_imgs:
-# 		print(img_name)
-# 		if img_name == ".DS_Store":
-# 			continue
-# 		img = ClImage(filename=f"data/train/glass/{img_name}", concepts=["glass"], not_concepts=["cardboard", "metal", "paper", "trash"])
-# 		imgs.append(img)
-# 	app.inputs.bulk_create_images(imgs)
+glass_imgs = os.listdir('data/train/glass')
+glass_imgs = list(chunks(glass_imgs, 128))
+for split_glass_imgs in glass_imgs:
+	imgs = []
+	for img_name in split_glass_imgs:
+		print(img_name)
+		if img_name == ".DS_Store":
+			continue
+		img = ClImage(filename=f"data/train/glass/{img_name}", concepts=["glass"], not_concepts=["cardboard", "metal", "paper", "trash"])
+		imgs.append(img)
+	app.inputs.bulk_create_images(imgs)
 
-# metal_imgs = os.listdir('data/train/metal')
-# metal_imgs = list(chunks(metal_imgs, 128))
-# for split_metal_imgs in metal_imgs:
-# 	imgs = []
-# 	for img_name in split_metal_imgs:
-# 		print(img_name)
-# 		if img_name == ".DS_Store":
-# 			continue
-# 		img = ClImage(filename=f"data/train/metal/{img_name}", concepts=["metal"], not_concepts=["glass", "cardboard", "paper", "plastic", "trash"])
-# 		imgs.append(img)
-# 	app.inputs.bulk_create_images(imgs)
+metal_imgs = os.listdir('data/train/metal')
+metal_imgs = list(chunks(metal_imgs, 128))
+for split_metal_imgs in metal_imgs:
+	imgs = []
+	for img_name in split_metal_imgs:
+		print(img_name)
+		if img_name == ".DS_Store":
+			continue
+		img = ClImage(filename=f"data/train/metal/{img_name}", concepts=["metal"], not_concepts=["glass", "cardboard", "paper", "plastic", "trash"])
+		imgs.append(img)
+	app.inputs.bulk_create_images(imgs)
 
-# paper_imgs = os.listdir('data/train/paper')
-# paper_imgs = list(chunks(paper_imgs, 128))
-# for split_paper_imgs in paper_imgs:
-# 	imgs = []
-# 	for img_name in split_paper_imgs:
-# 		print(img_name)
-# 		if img_name == ".DS_Store":
-# 			continue
-# 		img = ClImage(filename=f"data/train/paper/{img_name}", concepts=["paper"], not_concepts=["glass", "metal", "plastic", "trash"])
-# 		imgs.append(img)
-# 	app.inputs.bulk_create_images(imgs)
+paper_imgs = os.listdir('data/train/paper')
+paper_imgs = list(chunks(paper_imgs, 128))
+for split_paper_imgs in paper_imgs:
+	imgs = []
+	for img_name in split_paper_imgs:
+		print(img_name)
+		if img_name == ".DS_Store":
+			continue
+		img = ClImage(filename=f"data/train/paper/{img_name}", concepts=["paper"], not_concepts=["glass", "metal", "plastic", "trash"])
+		imgs.append(img)
+	app.inputs.bulk_create_images(imgs)
 
 trash_imgs = os.listdir('data/train/trash')
 trash_imgs = list(chunks(trash_imgs, 128))
