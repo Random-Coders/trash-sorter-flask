e19f26f1c0329d43875c31a8113d2f63c81939e2
Savagecoder77 <savagecoder77@gmail.com>
2019-03-09 18:06:48 -0800

    revert to regular flask

--- trash/__init__.py
+++ trash/__init__.py
@@ -4,8 +4,6 @@ Imports
 
 # Flask imports
 from flask import Flask
-from flask_restful import Api
-from trash import resources
 
 # Create Flask app
 app = Flask(__name__)
@@ -13,8 +11,4 @@ app = Flask(__name__)
 # Add Configurations to app
 app.config.from_pyfile('config.py', silent=True)
 
-api = Api(app)
-
-# create API endpoints
-api.add_resource(resources.PhotoUpload, '/api/trash/image/upload/<string:fname>')
-api.add_resource(resources.WhichBin, 'api/trash/sort/<item>')
+from trash import views
