becb5f4d97e7deac9db204ef998cf3462b6373e7
calee14 <caplee1000@gmail.com>
2019-10-13 12:03:07 -0700

    able to send img for prediction

--- trash/views.py
+++ trash/views.py
@@ -7,17 +7,25 @@ from PIL import Image
 import requests
 from io import BytesIO 
 from clarifai.rest import ClarifaiApp
+from clarifai.rest import Image as ClImage
 
 # app = Flask(__name__)
 
 capp = ClarifaiApp(api_key='fa54b65808c746f6a0d3a71cf2457bc9')
 model = capp.models.get('trashsorter')
 
-@app.route("/sort/<url>", methods=["GET", "POST"])
-def download(url):
-	print(url)
-	response = model.predict_by_url(url)
-	return json.dumps(response)
+@app.route("/sort", methods=["GET", "POST"])
+def sort():
+    if request.method == "POST":
+        if request.files:
+            img = Image.open(request.files['trash_pic'])
+            img.thumbnail((300,300), Image.ANTIALIAS)
+            f = path.join(app.config['UPLOAD_FOLDER'], 'image.png')
+            img.save(f)
+            res = model.predict_by_filename(path.join(app.config['UPLOAD_FOLDER'], 'image.png'))
+            print(res['output']['data'])
+            return 'success'
+    return render_template('index.html')
 
 @app.route("/", methods=["GET"])
 def home():
