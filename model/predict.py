58001a428563136bf67e38b4e01877fc892d5bd5
Savagecoder77 <savagecoder77@gmail.com>
2019-10-13 11:49:50 -0700

    changes

--- model/predict.py
+++ model/predict.py
@@ -6,6 +6,6 @@ app = ClarifaiApp(api_key=os.environ['CLARIFAICONSTANT'])
 model = app.models.get('trashsorter')
 # model.model_version = '1.0'  # This is optional. Defaults to the latest model version.
 
-response = model.predict_by_url('https://images-na.ssl-images-amazon.com/images/I/61SwuRMAPqL._SX342_.jpg')
+response = model.predict_by_url('https://github.com/Random-Coders/trash-sorter-flask/blob/raftesting/images/download.jpg?raw=true')
 # model.predict_by_filename()
 print(response)
