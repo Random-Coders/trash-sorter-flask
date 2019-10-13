e8df5b359c8b2625df5a19a0570097c46b220468
Savagecoder77 <savagecoder77@gmail.com>
2019-10-13 11:43:16 -0700

    images to test

--- model/predict.py
+++ model/predict.py
@@ -6,6 +6,6 @@ app = ClarifaiApp(api_key=os.environ['CLARIFAICONSTANT'])
 model = app.models.get('trashsorter')
 # model.model_version = '1.0'  # This is optional. Defaults to the latest model version.
 
-response = model.predict_by_url('http://www.recyclinggame.colostate.edu/images/chips.png')
+response = model.predict_by_url('https://images-na.ssl-images-amazon.com/images/I/61SwuRMAPqL._SX342_.jpg')
 # model.predict_by_filename()
 print(response)
